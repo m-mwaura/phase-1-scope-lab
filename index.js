3eb755e40bde9358704234e970fbbb39e84b8699
felix mwangi <fmwauramwangi@gmail.com>
2024-12-05 06:31:03 +0300

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Write your solution in this file!
+var customerName = 'bob'
+const leastFavoriteCustomer = ''
+
+/////////////////////////
+
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase()
+
+}
+
+////////////////////////////
+
+function setBestCustomer (){
+    bestCustomer = 'not bob'
+
+}
+
+///////////////////////////////
+
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob'
+
+}
+
+//////////////////////////////////
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'billy'
+
+}
+
